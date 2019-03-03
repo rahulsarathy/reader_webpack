2470cee86eec7a14bf99d4f93552e79ebf5f0b3d
Rahul Sarathy <rahul@sarathy.org>
2019-03-03 11:26:49 -0800

    flask communicating w/ js

--- server/server.py
+++ server/server.py
@@ -1,8 +1,17 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, Response
+import html_parser
+import json
 
 app = Flask(__name__, template_folder='../static', static_folder="../static/dist")
 
 @app.route('/')
 def hello_world(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/parse', methods=['POST'])
+def parse():
+	print(request.values)
+	res = json.dumps("hi")
+	r = Response(res, status=200)
+	return r
--- static/src/index.js
+++ static/src/index.js
@@ -5,8 +5,28 @@ function component() {
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack', 'blah3']);
-
+  element.id = "demo";
   return element;
 }
 
+function parse() {
+	let element = document.createElement('BUTTON');
+
+	element.innerHTML = 'parse';
+	element.onclick = callParse;
+	return element;
+}
+
+function callParse() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("demo").innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("POST", "/parse", true);
+	xhttp.send();
+}
+
 document.body.appendChild(component());
+document.body.appendChild(parse());
