4d46cbfd3b2e310e0ec9877f1e2019f91793cf15
Rahul Sarathy <rahul@sarathy.org>
2019-03-10 09:59:28 -0700

    removed old methods

--- server/server.py
+++ server/server.py
@@ -52,31 +52,6 @@ app = Flask(__name__, template_folder='../static', static_folder="../static/dist
 def main(name=None):
     return render_template('index.html', name=name)
 
-@app.route('/parse', methods=['POST'])
-def parse():
-	toParse = request.values.get('html')
-	print("result is " + toParse)
-	res = html_parser.parseHTML(toParse)
-	r = Response(res, status=200)
-	return r
-
-@app.route('/retrieve', methods=['POST'])
-def retrieve():
-	url = request.values.get('url')
-	print(url)
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-	toSend = req(url=url, headers=headers)
-	try:
-		html = urlopen(toSend).read()
-		clean = html_parser.parseHTML(html)
-		r = Response(clean, status=200)
-	except Exception as e:
-		print(e) 
-		response = json.dumps("invalid url")
-		r = Response(response, status=400)
-
-
-
 @app.route('/first', methods=['POST'])
 def findFirst():
 	print(sys.version)
