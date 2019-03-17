575111b9b7721c48032ac4607da229eae1cdbc6b
Rahul Sarathy <rahul@sarathy.org>
2019-03-16 22:55:39 -0700

    implemented models

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, Response
-import os
-
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder='../../static', static_folder="../../static/dist")
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
