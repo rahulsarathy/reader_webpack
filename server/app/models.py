b796b74e50e932b5b3f45565af9b86b6d5067044
Rahul Sarathy <rahul@sarathy.org>
2019-03-17 08:02:54 -0700

    blogs json column

--- server/app/models.py
+++ server/app/models.py
@@ -1,4 +1,18 @@
-from app import db
+from app import app, db, JSON
+from werkzeug.security import generate_password_hash, check_password_hash
+
+data = {
+	'stratechery': False,
+	'startupboy': False,
+	'bryan_caplan_econlib': False,
+	'marginal_revolution': False,
+	'ribbonfarm': False,
+'melting_asphalt': False,
+	'overcoming_bias': False,
+	'elaine_ou': False,
+	'eugene_wei': False,
+	'meaningness': False
+}
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,9 +20,17 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     kindle_email = db.Column(db.String(120), index=True, unique=True)
-    stratechery = db.Column(db.Boolean, index=True, default=False)
-    startupboy = db.Column(db.Boolean, index=True, default=False)
+    blogs = db.Column(JSON, default=lambda: data )
 
+    def set_password(self, password):
+    	self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+    	return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': Users}
